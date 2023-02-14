de70d6594a33daa0b1c085641fbb44c14d6518c3
Raul Jimenez <rjbarco@gmail.com>
2023-02-14 12:05:05 +0100

    Added getters and setters to Node factory. Added 'head', 'tail', 'size' and 'append' methods to LinkedList

--- node.js
+++ node.js
@@ -5,7 +5,23 @@ const Node = () => {
     let _nextNode = null;
 
     // Methods
-    const value = () => _value;
+    
+    // Return value field of a node
+    const getValue = () => _value;
 
-    const nextNode = () => _nextNode;
+    // Return a link to the next node in the linked list
+    const getNextNode = () => _nextNode;
+
+    // Set value to input value
+    const setValue = (value) => _value = value;
+    
+    // Sel nextNode to input node
+    const setNextNode = (node) => _nextNode =  node;
+
+    return {
+        getValue,
+        getNextNode,
+        setValue,
+        setNextNode
+    }
 };
---
+++ tests.js
@@ -0,0 +1,2 @@
+import "./linked-list";
+
